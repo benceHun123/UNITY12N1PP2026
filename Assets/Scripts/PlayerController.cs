33fac30d6202f135b4da68b15a33d67e7538dc18
benceHun123 <bence.viragh16@gmail.com>
2026-02-20 13:16:22 +0100

    asd

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _maxSpeed = 5;
+    [SerializeField] float _jumpHeight = 500;
+
+    [SerializeField] float _groundCheckRadius = .1f;
+    
+    [SerializeField] LayerMask _groundLayer;
+    [SerializeField] Transform _groundCheker;
 
     Animator _animator;
     Rigidbody2D _rigidbody;
 
     bool facingRight;
+    private bool isGrounded;
 
     private void Start()
     {
@@ -16,10 +23,27 @@ public class PlayerController : MonoBehaviour
         _animator = GetComponent<Animator>();
 
         facingRight = true;
+        isGrounded = false;
+
+    }
+
+    private void Update()
+    {
+        if (isGrounded && Input.GetAxis("Jump") > 0)
+        {
+            _animator.SetBool("isGrounded", false);
+            isGrounded = false;
+            _rigidbody.AddForce(new Vector2(0, _jumpHeight));
+        }
     }
 
     private void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapCircle(_groundCheker.position, _groundCheckRadius, _groundLayer);
+        _animator.SetBool("isGrounded", isGrounded);
+        _animator.SetFloat("verticalSpeed", _rigidbody.linearVelocity.y);
+
+
         float move = Input.GetAxis("Horizontal");
 
         _animator.SetFloat("horizontalSpeed",Mathf.Abs(move));
